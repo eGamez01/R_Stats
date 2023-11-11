a7d459306e02f42646d03f14b8a1fa6cd8b5a349
cld3033 <cldennington@gmail.com>
2023-11-10 19:58:23 -0600

    Removed extra characters from IDATE

--- data_exploration.R
+++ data_exploration.R
@@ -1,3 +1,10 @@
 #reading in reduced 2015 dataset. 20% sample of original file.
 data <- read.csv("https://raw.githubusercontent.com/eGamez01/lfs_data/master/reduced_2015.csv")
 
+# copy of data to test cleaning with 
+data2 <- data
+
+# remove first character (b) and last character (') from IDATE 
+data2$IDATE <- gsub('^.|.$', '', data2$IDATE)
+# remove first character
+data2$IDATE <- sub('.', '', data2$IDATE)
