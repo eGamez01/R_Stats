d811afe94b398732d26b8274a2130980cf785cef
cld3033 <cldennington@gmail.com>
2023-11-17 19:03:50 -0600

    Test comment

--- EG_data_exploration.R
+++ EG_data_exploration.R
@@ -1,4 +1,10 @@
 #reading in 2015 cleansed data set. 
 data <- read.csv("https://raw.githubusercontent.com/eGamez01/lfs_data/master/2015_diabetes_cleansed.csv")
 
+<<<<<<< HEAD
 #comment
+=======
+#comment
+
+#comment by Caroline #
+>>>>>>> 70a5986 (Test comment)
