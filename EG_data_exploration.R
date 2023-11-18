3f80e17636954155a376a3cd110a1de24a7acc63
Eduardo Gamez <eduardo.gamez.eng@gmail.com>
2023-11-17 19:02:34 -0600

    adding new link to read in data for 2015 cleansed

--- EG_data_exploration.R
+++ EG_data_exploration.R
@@ -1,5 +1,5 @@
 #reading in reduced 2015 dataset. 20% sample of original file.
-data <- read.csv("https://raw.githubusercontent.com/eGamez01/lfs_data/master/reduced_2015.csv")
+data <- read.csv("https://raw.githubusercontent.com/eGamez01/lfs_data/master/2015_diabetes_cleansed.csv")
 
 #comment
 #comment2
