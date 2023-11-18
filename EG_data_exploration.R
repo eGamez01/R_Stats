ca2c2beed68e2cd8c65849a37469283a6463ca42
Eduardo Gamez <eduardo.gamez.eng@gmail.com>
2023-11-17 19:11:59 -0600

    adding example of Non-metric MDS

--- EG_data_exploration.R
+++ EG_data_exploration.R
@@ -1,5 +1,7 @@
-#reading in reduced 2015 dataset. 20% sample of original file.
+#reading in 2015 cleansed data set. 
 data <- read.csv("https://raw.githubusercontent.com/eGamez01/lfs_data/master/2015_diabetes_cleansed.csv")
 
 #comment
 #comment2
+
+
